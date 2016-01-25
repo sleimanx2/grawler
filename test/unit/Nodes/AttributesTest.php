ba4c27cf8af0b7b9dc8867668bbe4f5f99138a32
sleimanx2 <sleiman@outlook.com>
2016-01-25 18:43:14 +0200

    removed some attributes tests

--- test/unit/Nodes/AttributesTest.php
+++ test/unit/Nodes/AttributesTest.php
@@ -44,74 +44,7 @@ class AttributesTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(['id','title'],$attributesSubclass->getKeys());
     }
 
-    /** @test */
-    function it_can_return_all_attributes_as_array()
-    {
-        $attributesSubclass = new AttributeSubClass();
-        $this->assertEquals([
-            'id'    => '21-22-23',
-            'title' => 'subclass title'
-        ],$attributesSubclass->asArray());
 
-        $attributesSubclass = new AttributeSubClass();
-        $this->assertEquals([
-            'id'    => '21-22-23',
-            'title' => 'subclass title'
-        ],$attributesSubclass->all());
-    }
-
-    /** @test */
-    function it_can_return_the_number_attributes()
-    {
-        $attributesSubclass = new AttributeSubClass();
-        $this->assertEquals(2,$attributesSubclass->count());
-    }
-
-
-
-    /** @test */
-    function it_returns_true_if_the_offset_exits()
-    {
-        $attributesSubclass = new AttributeSubClass();
-        $this->assertTrue($attributesSubclass->offsetExists('id'));
-    }
-
-    /** @test */
-    function it_returns_false_if_the_offset_is_not_available()
-    {
-        $attributesSubclass = new AttributeSubClass();
-        $this->assertFalse($attributesSubclass->offsetExists('body'));
-    }
-
-//    /** @test */
-//    function it_can_return_an_iterator_instance()
-//    {
-//        $attributesSubclass = new AttributeSubClass();
-//        $this->assertInstanceOf(ArrayIterator::class,$attributesSubclass->getIterator());
-//    }
-//    /** @test */
-//    function offset_get_is_an_alias_for_getAttribute()
-//    {
-//        $mock = $this->getMock(AttributeSubClass::class,['getAttribute']);
-//        $mock->expects($this->once())->method('getAttribute');
-//        $mock->offsetGet('title');
-//    }
-//
-//    /** @test */
-//    function offset_set_is_an_alias_for_setAttribute()
-//    {
-//        $mock = $this->getMock(AttributeSubClass::class,['setAttribute']);
-//        $mock->expects($this->once())->method('setAttribute');
-//        $mock->offsetSet('title','krekib');
-//    }
-//
-//    /** @test */
-//    function offset_unset_is_an_alias_for_setAttribute_to_null()
-//    {
-//        $mock = $this->getMock(AttributeSubClass::class,['setAttribute']);
-//        $mock->expects($this->once())->method('setAttribute')->with('title');
-//        $mock->offsetUnset('title');
-//    }
 }
 
 
